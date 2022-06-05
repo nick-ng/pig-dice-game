c442c3b400cb9e70268ceff636cf4e5c37697eb3
Nick Ng <nicholas.cj.ng@gmail.com>
2022-06-05 20:08:32 +1200

    halp

---
+++ src-front/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+export default function App() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
