3157e1d82407db5605e4d3a9c5bd619eef54d442
Nick Ng <nicholas.cj.ng@gmail.com>
2022-06-11 20:20:40 +1200

    add script to deploy to git hub pages

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/bash
 
+git checkout main
+git pull
 npm run build-front
 cp ./dist-front/index.html ./dist-front/404.html
 cp ./static/* ./dist-front
 echo \"pig-dice-game.pux.one\" > ./dist-front/CNAME
 
+git checkout --orphan gh-pages
+git reset
 cp ./dist-front/* ./
+git add $(ls ./dist-front)
+
+git commit -m "$(date) deploy front-end"
+
+git push origin gh-pages
+
+git add .
+git commit -m "$(date) throw away"
+
+git checkout main
+
+git diff --name-status main test
